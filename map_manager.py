e56d2bc8e3178b9a69cc747ac4101f55efc5963c
joe Defendre <jchacker5@gmail.com>
2023-03-27 16:15:48 -0400

    modified:   map_manager.py 	modified:   reduce_manager.py

--- map_manager.py
+++ map_manager.py
@@ -1,8 +1,15 @@
 import grpc
+from concurrent import futures
 from word_count_pb2 import *
 from word_count_pb2_grpc import *
 
-class MapManager:
+class MapperTask:
+    def __init__(self, input_line):
+        self.input_line = input_line
+
+    # Implement other methods for MapperTask
+
+class MapManager(MapManagerServicer):
     def __init__(self):
         self.mapper_tasks = []
 
@@ -11,16 +18,17 @@ class MapManager:
         self.mapper_tasks.append(mapper_task)
         return mapper_task
 
-    def serve():
-        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        add_MapManagerServicer_to_server(MapManager(), server)
-        server.add_insecure_port('[::]:50052')
-        server.start()
-        print('Map Manager node started on port 50052')
-        server.wait_for_termination()
     def CreateMapTask(self, request, context):
         # Implement the gRPC method for creating a mapper task
         pass
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    add_MapManagerServicer_to_server(MapManager(), server)
+    server.add_insecure_port('[::]:50052')
+    server.start()
+    print('Map Manager node started on port 50052')
+    server.wait_for_termination()
 
 if __name__ == "__main__":
     serve()
